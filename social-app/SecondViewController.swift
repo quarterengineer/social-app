bf63d4dc2ab1f421ebe142996073d829d8293c77
dogukandemirci1997 <15542008@firat.edu.tr>
2018-10-29 23:45:06 +0300

    Initial Commit

---
+++ social-app/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  social-app
+//
+//  Created by @unknown on 29.10.2018.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
